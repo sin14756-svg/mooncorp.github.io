c93a9da22364d5a8b92f15c36f9a3254ddb28b4e
sin14756-svg <sin14756@gmail.com>
2026-06-05 03:23:13 +0700

    CMS Auto Sync: update cms-text-config.js

--- cms-text-config.js
+++ cms-text-config.js
@@ -7,7 +7,7 @@
 
 var CMS_TEXT_CONFIG = {
     "contact-email": "sin14756@gmail.com",
-    "home-hero-label": "FRESH FROM THE SEA66",
+    "home-hero-label": "FRESH FROM THE SEA69",
     "home-hero-title": "CV. GOLDEN SEAFRESH",
     "home-hero-sub": "FROZEN SEAFOOD",
     "home-hero-desc": "CV. Golden Seafresh is a manufacturer and distributor of high-quality frozen seafood, committed to delivering freshness from the sea to customers both domestically and internationally.<br><br>We rigorously select fresh seafood directly from natural sources, preserving freshness, taste, and nutritional value at every stage — under HACCP and GMP international standards.",
@@ -47,5 +47,5 @@ var CMS_TEXT_CONFIG = {
     "product-8-size": "500/800 - 800/1000 - 1000/1300 - 1300/1500 - 1500/1800",
     "product-9-type": "Whole round / GGS",
     "product-9-size": "60/80 - 80/100 - 100/150 - 150/200 - 200/250 - 250/300",
-    "_version": 1780604434629
+    "_version": 1780604592937
 };
