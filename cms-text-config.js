d797deffc9ada590e709012911d856686dfa0a86
sin14756-svg <sin14756@gmail.com>
2026-06-12 15:00:56 +0700

    CMS Auto Sync: update cms-text-config.js

--- cms-text-config.js
+++ cms-text-config.js
@@ -6,10 +6,10 @@
  * ========================================================= */
 
 var CMS_TEXT_CONFIG = {
-    "show-activities": "false",
-    "show-about-gallery": "true",
     "logo-header-height": "58",
     "logo-footer-height": "58",
+    "show-activities": "false",
+    "show-about-gallery": "false",
     "contact-email": "goldenseafresh.marketing@gmail.com",
     "home-hero-label": "FRESH FROM THE SEA5",
     "home-hero-title": "CV. GOLDEN SEAFRESH8",
@@ -51,5 +51,5 @@ var CMS_TEXT_CONFIG = {
     "product-8-size": "500/800 - 800/1000 - 1000/1300 - 1300/1500 - 1500/1800",
     "product-9-type": "Whole round / GGS",
     "product-9-size": "60/80 - 80/100 - 100/150 - 150/200 - 200/250 - 250/300",
-    "_version": 1781249569000
+    "_version": 1781251131457
 };
