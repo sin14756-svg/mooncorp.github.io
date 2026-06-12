8a63878a1c09bfadac1af1132128e2b7a5d19655
sin14756-svg <sin14756@gmail.com>
2026-06-12 15:56:28 +0700

    CMS Auto Sync: update image-config.js

--- image-config.js
+++ image-config.js
@@ -25,7 +25,7 @@ var IMAGE_CONFIG = {
     "cert-haccp": {
         "x": 0,
         "y": 0,
-        "zoom": 1.05,
+        "zoom": 1,
         "aspectRatio": "original",
         "src": "pic/uploads/img_cert-haccp_1781252327920.png"
     },
@@ -386,5 +386,5 @@ var IMAGE_CONFIG = {
         "aspectRatio": "original",
         "src": "pic/service/5.png"
     },
-    "_version": 1781254345139
+    "_version": 1781254583910
 };
