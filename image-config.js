96e0c9125aeb838de1bd6fbb10dfd61e1c4b5ab7
sin14756-svg <sin14756@gmail.com>
2026-06-12 16:33:06 +0700

    CMS Auto Sync: update image-config.js

--- image-config.js
+++ image-config.js
@@ -130,7 +130,7 @@ var IMAGE_CONFIG = {
     "vision-bg": {
         "x": -4,
         "y": -109,
-        "zoom": 1.05,
+        "zoom": 1.1,
         "aspectRatio": "16/9",
         "src": "pic/uploads/img_vision-bg_1781256577035.png"
     },
@@ -386,5 +386,5 @@ var IMAGE_CONFIG = {
         "aspectRatio": "original",
         "src": "pic/service/5.png"
     },
-    "_version": 1781256683712
+    "_version": 1781256781628
 };
